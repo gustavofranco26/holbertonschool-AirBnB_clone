f2ce58b63f6aea67e992c8ecd868359f07d12315
Cesar240998 <cesar.evanan@utec.edu.pe>
2022-10-16 22:10:54 -0500

    Revert "adding tests folder"
    This reverts commit a303e37cef74648bf38b9f98756421f750362f30.

---
+++ tests/test_models/test_review.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Testing Review class"""
+from tests.test_models.test_base_model import test_basemodel
+from models.review import Review
+
+
+class test_review(test_basemodel):
+    """ Testing review"""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.name = "Review"
+        self.value = Review
+
+    def test_place_id(self):
+        """ Testing place id"""
+        new = self.value()
+        self.assertEqual(type(new.place_id), str)
+
+    def test_user_id(self):
+        """ Testing user id"""
+        new = self.value()
+        self.assertEqual(type(new.user_id), str)
+
+    def test_text(self):
+        """ Testing test"""
+        new = self.value()
+        self.assertEqual(type(new.text), str)
